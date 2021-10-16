5bc99f519f576432c738b93d7e5f120182e14fe0
Jainish Shah <jainish.shah@workday.com>
2021-10-16 01:37:54 -0700

    updating blogs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -22,22 +22,22 @@ jQuery(document).ready(function() {
 		scroll_to($(this), $('nav').outerHeight());
 	});
 	// toggle "navbar-no-bg" class
-	$('.top-content .text').waypoint(function() {
+	$('.home-content .text').waypoint(function() {
 		$('nav').toggleClass('navbar-no-bg');
 	});
 	
     /*
         Background slideshow
     */
-    $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
+    $('.home-content').backstretch("assets/img/backgrounds/1.jpg");
     $('.call-to-action-container').backstretch("assets/img/backgrounds/1.jpg");
     $('.testimonials-container').backstretch("assets/img/backgrounds/1.jpg");
     
     $('#top-navbar-1').on('shown.bs.collapse', function(){
-    	$('.top-content').backstretch("resize");
+    	$('.home-content').backstretch("resize");
     });
     $('#top-navbar-1').on('hidden.bs.collapse', function(){
-    	$('.top-content').backstretch("resize");
+    	$('.home-content').backstretch("resize");
     });
     
     $('a[data-toggle="tab"]').on('shown.bs.tab', function(){
